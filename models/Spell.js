eaa1090b44c75b98c2d5f22faca4e5c82617ba0f
Zachary Ryan <zacharyryan@Zachs.local>
2020-04-16 11:36:03 -0400

    updated model to Spell

--- models/Spell.js
+++ models/Spell.js
@@ -1,3 +1,7 @@
+// see if spell exists with "index"
+// create a new Spell from findSpellBySpellIndex w/ spellName = 'findSpellBySpellIndex--result.name'
+// display that spell in the charController
+
 Char.findSpellBySpellIndex = function(spellIndex) {
     return new Promise(function(resolve, reject) {
         if (typeof(spellIndex) != "string") {
@@ -6,3 +10,4 @@ Char.findSpellBySpellIndex = function(spellIndex) {
         }
     }).then().catch()
 }
+
